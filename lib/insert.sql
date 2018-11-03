637889c603c44b7b26c87a92457a6ba471f6ee67
Rachel Wyatt <rw126@evansville.edu>
2018-11-03 14:40:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'Female', 'Brown', 'Angsty', 1),
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'Female', 'Brown', 'Angsty', 1),
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Brown', 'Angsty', 1),
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'Female', 'Brown', 'Angsty', 1),
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'Female', 'Brown', 'Angsty', 1),
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'Female', 'Brown', 'Angsty', 1),
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'Brown', 'Angsty', 1),
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'Brown', 'Angsty', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NIL, 2, 'Female', 'Brown', 'Angsty', 1);
