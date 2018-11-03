2ebafc0ccff7160205296d28b59bf9825ae050bb
Rachel Wyatt <rw126@evansville.edu>
2018-11-03 14:40:51 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2, 'Female', 'Brown', 'Angsty', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 2, 'Female', 'Brown', 'Angsty', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Female', 'Brown', 'Angsty', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('NIL', 2, 'Female', 'Brown', 'Angsty', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2, 'Female', 'Brown', 'Angsty', 1);
